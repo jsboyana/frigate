8a6f928f3aa70426c789a8125496b70b0a60c52f
Jitendra Singh Rao <jitendra.rao@kadellabs.com>
2025-03-03 15:38:42 +0530

    cookie issue remove

--- web/src/utils/cookieUtil.ts
+++ web/src/utils/cookieUtil.ts
@@ -1,11 +1,13 @@
 export const setCookie = (name: string, value: string, hours: number) => {
     const expires = new Date();
     expires.setTime(expires.getTime() + hours * 60 * 60 * 1000);
-    document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; Secure; SameSite=Strict`;
+    const secureFlag = window.location.protocol === "https:" ? "Secure;" : "";
+    document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; ${secureFlag} SameSite=Strict`;
 };
 
 export const removeCookie = (name: string) => {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; Secure; SameSite=Strict`;
+    const secureFlag = window.location.protocol === "https:" ? "Secure;" : "";
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; ${secureFlag} SameSite=Strict`;
 };
 
 export const getCookie = (name: string): string | null => {
@@ -13,3 +15,4 @@ export const getCookie = (name: string): string | null => {
     const authCookie = cookies.find((row) => row.startsWith(`${name}=`));
     return authCookie ? authCookie.split("=")[1] : null;
 };
+
